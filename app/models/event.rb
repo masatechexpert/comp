3d9b27f9ea7b48176beacf52de706c31d228f1b1
masatechexpert <cate35cate@gmail.com>
2019-09-05 06:03:25 +0900

    validates

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :title, length: { maximum: 40 }
-  validates :description, length: { maximum: 500 }
+  validates :title, length: { maximum: 40 }, presence: true
+  validates :description, length: { maximum: 500 }, presence: true
   validates :start_date, presence: true
   geocoded_by :address
   after_validation :geocode
